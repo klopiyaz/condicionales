4f70e9ad9a9ffee4a509f86031a9fbcf9d4b565a
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-15 14:12:03 -0500

    Add files via upload

---
+++ Tarifas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario");
+            int salario = int.Parse(Console.ReadLine());
+
+            int smmlv = 877800;
+
+            if (salario < 2 * smmlv)
+            {
+                Console.WriteLine("Tarifa A");
+            }
+            else if (2 * smmlv <= salario && salario <= 4 * smmlv)
+            {
+                Console.WriteLine("Tarifa B");
+            }
+            else if (salario > 4 * smmlv)
+            {
+                Console.WriteLine("Tarifa c");
+            }
+
+
+        }
+    }
+}
