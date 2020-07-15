b37a9da030ada610eea0a964765213c8d3dbbc72
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-15 14:09:50 -0500

    Add files via upload

---
+++ trabajo de IMC.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su estatura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            if (imc < 18)
+            {
+                Console.WriteLine("bajo peso...");
+            }
+            else if (18 <= imc && imc <= 25)
+            {
+                Console.WriteLine("peso ideal...");
+            }
+            else if (25 <= imc && imc <= 29)
+            {
+                Console.WriteLine("sobrepeso...");
+            }
+            else if (30 <= imc && imc <= 35)
+            {
+                Console.WriteLine("obesidad...");
+            }
+            else if (imc > 35)
+            {
+                Console.WriteLine("obesidad morbida");
+            }
+            Console.WriteLine("IMC es: " + imc);
+        }
+    }
+}
